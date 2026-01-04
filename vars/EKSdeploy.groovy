4265a99d968e534e055cdff8f7b394046e52f5f3
Akhil Naidu <akhilnaid1997@gmail.com>
2026-01-04 17:52:26 +0530

    jenkins

--- vars/EKSdeploy.groovy
+++ vars/EKSdeploy.groovy
@@ -34,6 +34,7 @@ def call (Map configmap) {
                                 ls -l
                                 echo ${appVersion}
                                 sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
+                                cat values.yaml
                                 helm upgrade --install ${component} -f values-${ENVIRONMENT}.yaml -n ${project} --atomic --wait --timeout=5m .
                             """
                         }
